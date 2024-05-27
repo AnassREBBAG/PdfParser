b97f26b5a7aa59c2437b5dbf1cc3344b46822a37
Anass REBBAG <95964342+AnassREBBAG@users.noreply.github.com>
2024-05-27 23:40:09 +0000

    removed Builder class and methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,20 +7,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class Main {
 
-
-
     public static boolean questionFound, optionAFound, OptionBFound, optionCFound,
-            optionDFound, optionEFound, correctAnswerFound ;
+            optionDFound, optionEFound, correctAnswerFound;
 
-
-    static Question group ;
+    // static Question group ;
 
     public static void main(String[] args) {
-        //String filePath = "src/main/resources/Test_PDF_Extractor3.pdf";
-        String filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "Test_PDF_Extractor3.pdf";
+        // String filePath = "src/main/resources/Test_PDF_Extractor3.pdf";
+        String filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator
+                + "Test_PDF_Extractor3.pdf";
 
         ArrayList<Question> questions = new ArrayList<>();
 
@@ -28,49 +25,54 @@ public class Main {
             PDFTextStripper pdfStripper = new PDFTextStripper();
             String text = pdfStripper.getText(document);
 
-
             String[] paragraphList = text.split("\n");
-
-
-
 
             int i = 0;
 
-            while(i < paragraphList.length){
+            while (i < paragraphList.length) {
 
-                if (Parser.findQuestion(paragraphList[i])){
+                if (Parser.findQuestion(paragraphList[i])) {
 
-                    group = new Question.Builder()
-                            .optionA(Parser.extractOption(paragraphList[i+2]))
-                            .optionB(Parser.extractOption(paragraphList[i+3]))
-                            .optionC(Parser.extractOption(paragraphList[i+4]))
-                            .optionD(Parser.extractOption(paragraphList[i+5]))
-                            .correctAnswer(Parser.extractCorrectAnswer(paragraphList[i+6]))
-                            .build();
+                    // Question group = new Question.Builder()
+                    // .optionA(Parser.extractOption(paragraphList[i+2]))
+                    // .optionB(Parser.extractOption(paragraphList[i+3]))
+                    // .optionC(Parser.extractOption(paragraphList[i+4]))
+                    // .optionD(Parser.extractOption(paragraphList[i+5]))
+                    // .correctAnswer(Parser.extractCorrectAnswer(paragraphList[i+6]))
+                    // .build();
 
+                    Question group = new Question();
+                    group.questionText = Parser.extractQuestion(paragraphList[i + 1]);
+                    group.optionA = Parser.extractQuestion(paragraphList[i + 2]);
+                    group.optionB = Parser.extractQuestion(paragraphList[i + 3]);
+                    group.optionC = Parser.extractQuestion(paragraphList[i + 4]);
+                    group.optionD = Parser.extractQuestion(paragraphList[i + 6]);
+                    // group.optionE = Parser.extractQuestion(paragraphList[i+6]);
+                    group.correctAnswer = Parser.extractCorrectAnswer(paragraphList[i + 6]);
 
-                    System.out.println(group.toString());
-//                    System.out.println(paragraphList[i+1]);
-//                    System.out.println(paragraphList[i+2]);
-//                    System.out.println(paragraphList[i+3]);
-//                    System.out.println(paragraphList[i+4]);
-//                    System.out.println(paragraphList[i+5]);
-//                    System.out.println(paragraphList[i+6]);
+                    questions.add(group);
+                    // System.out.println(group.toString());
+                    // System.out.println(paragraphList[i+1]);
+                    // System.out.println(paragraphList[i+2]);
+                    // System.out.println(paragraphList[i+3]);
+                    // System.out.println(paragraphList[i+4]);
+                    // System.out.println(paragraphList[i+5]);
+                    // System.out.println(paragraphList[i+6]);
 
                 }
                 i++;
 
             }
 
-            //todo : write parsing code
+            // todo : write parsing code
 
+            questions.forEach(group -> System.out.println(group.toString()));
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(group.toString());
+        // System.out.println(group.toString());
         System.out.println("#################################");
 
     }
--- src/main/java/org/example/Question.java
+++ src/main/java/org/example/Question.java
@@ -1,92 +1,39 @@
 package org.example;
 
 public class Question {
-    private String questionText;
-    private String optionA;
-    private String optionB;
-    private String optionC;
-    private String optionD;
-    private String optionE;
-    private String optionF;
-    private String correctAnswer;
+    public String optionA;
+    public String optionB;
+    public String optionC;
+    public String optionD;
+    public String optionE;
+    public String optionF;
+    public String correctAnswer;
+    public String questionText;
 
-    private Question(Builder builder) {
-        this.questionText = builder.questionText;
-        this.optionA = builder.optionA;
-        this.optionB = builder.optionB;
-        this.optionC = builder.optionC;
-        this.optionD = builder.optionD;
-        this.optionE = builder.optionE;
-        this.optionF = builder.optionF;
-        this.correctAnswer = builder.correctAnswer;
-    }
+    public Question() {
 
-    public static class Builder {
-        private String questionText;
-        private String optionA;
-        private String optionB;
-        private String optionC;
-        private String optionD;
-        private String optionE;
-        private String optionF;
-        private String correctAnswer;
+        this.questionText = " ";
+        this.optionA = " ";
+        this.optionB = " ";
+        this.optionC = " ";
+        this.optionD = " ";
+        this.optionE = " ";
+        this.optionF = " ";
+        this.correctAnswer = " ";
 
-        public Builder questionText(String questionText) {
-            this.questionText = questionText;
-            return this;
-        }
-
-        public Builder optionA(String optionA) {
-            this.optionA = optionA;
-            return this;
-        }
-
-        public Builder optionB(String optionB) {
-            this.optionB = optionB;
-            return this;
-        }
-
-        public Builder optionC(String optionC) {
-            this.optionC = optionC;
-            return this;
-        }
-
-        public Builder optionD(String optionD) {
-            this.optionD = optionD;
-            return this;
-        }
-
-        public Builder optionE(String optionE) {
-            this.optionE = optionE;
-            return this;
-        }
-
-        public Builder optionF(String optionF) {
-            this.optionF = optionF;
-            return this;
-        }
-
-        public Builder correctAnswer(String correctAnswer) {
-            this.correctAnswer = correctAnswer;
-            return this;
-        }
-
-        public Question build() {
-            return new Question(this);
-        }
     }
 
     @Override
     public String toString() {
         return "Question{" + '\n' +
                 "questionText='" + this.questionText + '\'' + '\n' +
-                ", optionA='" + this.optionA + '\'' + '\n' +
-                ", optionB='" + this.optionB + '\'' +'\n' +
-                ", optionC='" + this.optionC + '\'' +'\n' +
-                ", optionD='" + this.optionD + '\'' +'\n' +
-                ", optionE='" + this.optionE + '\'' +'\n' +
-                ", optionF='" + this.optionF + '\'' +'\n' +
-                ", correctAnswer='" + this.correctAnswer + '\'' +'\n' +
-                '}' + '\n' ;
+                "optionA='" + this.optionA + '\'' + '\n' +
+                "optionB='" + this.optionB + '\'' + '\n' +
+                "optionC='" + this.optionC + '\'' + '\n' +
+                "optionD='" + this.optionD + '\'' + '\n' +
+                "optionE='" + this.optionE + '\'' + '\n' +
+                "optionF='" + this.optionF + '\'' + '\n' +
+                "correctAnswer='" + this.correctAnswer + '\'' + '\n' +
+                '}' + '\n';
     }
 }
