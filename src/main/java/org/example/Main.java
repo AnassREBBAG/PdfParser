2ee7c8c58408c034474fe270cdd95238dc50b924
Anass REBBAG <95964342+AnassREBBAG@users.noreply.github.com>
2024-06-11 23:01:47 +0000

    removed explanation, numbers added, errors fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,36 +7,33 @@ public class Main {
         System.out.println("starting ..");
 
         String content = ParserUtils.extractTextFromFile("src/main/resources/IMPUTV2.txt");
-        
-        
 
-        ParserUtils.createCSVFile("Output", "Question", "QuestionType", 
-        "Answer Option 1", "Explanation 1",
-        "Answer Option 2", "Explanation 2",
-        "Answer Option 3", "Explanation 3",
-        "Answer Option 4", "Explanation 4",
-        "Answer Option 5", "Explanation 5",
-        "Answer Option 6", "Explanation 6",
-        "Correct Answer", "Overall Explanation",
-        "Domain"
-        ) ;
-         
-         
-         //ParserUtils.extractQuestions(content).forEach(q -> System.out.println(q.toString()));
+        ParserUtils.createCSVFile("Output", "Question", "QuestionType",
+                "Answer Option 1", "Explanation 1",
+                "Answer Option 2", "Explanation 2",
+                "Answer Option 3", "Explanation 3",
+                "Answer Option 4", "Explanation 4",
+                "Answer Option 5", "Explanation 5",
+                "Answer Option 6", "Explanation 6",
+                "Correct Answer", "Overall Explanation",
+                "Domain");
 
-        
+        // ParserUtils.extractQuestions(content).forEach(q ->
+        // System.out.println(q.toString()));
+
         List<Question> questions = ParserUtils.extractQuestions(content);
 
-        questions.forEach(q -> ParserUtils.writeToCSVFile("Output.csv", q.questionText, "no type", 
-        q.option1, q.explanation1, 
-        q.option2, q.explanation2, 
-        q.option3, q.explanation3,
-        q.option4, q.explanation4, 
-        q.option5, q.explanation5, 
-        q.option6, q.explanation6,
-        q.correctAnswer, q.overallExplanation,
-        q.domain ));
+        questions.forEach(QuestionType::setQuestionType);
 
+        questions.forEach(q -> ParserUtils.writeToCSVFile("Output.csv", q.questionText, "no type",
+                q.option1, q.explanation1,
+                q.option2, q.explanation2,
+                q.option3, q.explanation3,
+                q.option4, q.explanation4,
+                q.option5, q.explanation5,
+                q.option6, q.explanation6,
+                q.correctAnswer, q.overallExplanation,
+                q.domain));
 
     }
 }
